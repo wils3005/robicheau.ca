f89ebb73b02c2212e36b86cf84564e697236193e
Jack Wilson <jack@swiftmedical.io>
2021-03-03 22:17:42 -0500

    Thu, 04 Mar 2021 03:17:42 GMT
     31 files changed, 7042 insertions(+), 2209 deletions(-)

--- docs/_next/static/JbLpgx2p-YFm5L-5zguaA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-7aeee745a2da7f051311.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ca3c9e1107d05fd508ac.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
