56f539ebacc77ab170b9347228acb48e8926027d
Jack Wilson <jack@swiftmedical.io>
2021-03-03 22:54:37 -0500

    Thu, 04 Mar 2021 03:54:37 GMT
     6 files changed, 16 insertions(+), 22 deletions(-)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,17 @@ function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Robicheau.ca Title</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!!!!! !</a>
-        </h1>
+        <hgroup>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://robicheau.ca">robicheau.ca!</a>
+          </h1>
+          <h2>Secondary blah blah...</h2>
+        </hgroup>
 
         <p className={styles.description}>
           Get started by editing{" "}
@@ -20,12 +23,12 @@ function Home(): JSX.Element {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://robicheau.ca/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://robicheau.ca/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
@@ -41,24 +44,12 @@ function Home(): JSX.Element {
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          ></a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>top level footer</p>
       </footer>
     </div>
   );
