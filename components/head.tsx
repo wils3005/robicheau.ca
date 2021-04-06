b2f3a80fbf7e40ea6496094fe87d44fa742e42b8
Jack Wilson <jack@swiftmedical.io>
2021-04-05 23:09:56 -0400

    Tue, 06 Apr 2021 03:09:56 GMT
     7 files changed, 20 insertions(+)

--- components/head.tsx
+++ components/head.tsx
@@ -9,6 +9,26 @@ function main(): JSX.Element {
       <link href="favicon.ico" rel="icon" />
 
       <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="apple-touch-icon.png"
+      />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon-32x32.png"
+      />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon-16x16.png"
+      />
+
+      <link
         href="//fonts.googleapis.com/css?family=Source+Serif+Pro:400,600,700|Roboto:300,400,400i,500,700"
         rel="stylesheet"
       />
