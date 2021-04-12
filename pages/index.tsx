18a6d3aa2f62c09764a5e1f9f3b67399c69b3f63
Jack Wilson <jack@swiftmedical.io>
2021-04-12 13:23:08 -0400

    Mon, 12 Apr 2021 17:23:08 GMT
     1 file changed, 1 insertion(+), 19 deletions(-)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,5 @@
-import Footer from "../components/footer";
-import Head from "../components/head";
-import Header from "../components/header";
-import HeroImage from "../components/hero-image";
-import MeetNicole from "../components/meet-nicole";
-import SignUpForUpdates from "../components/sign-up-for-updates";
-import WhatICareAbout from "../components/what-i-care-about";
-
 function main(): JSX.Element {
-  return (
-    <main>
-      <Head />
-      <Header />
-      <HeroImage />
-      <MeetNicole />
-      <WhatICareAbout />
-      <SignUpForUpdates />
-      <Footer />
-    </main>
-  );
+  return <main></main>;
 }
 
 export default main;
