2f47e091209e503f6866049dbeaa1966735830f1
Jack Wilson <jackwilson@Jacks-MacBook-Pro.local>
2021-06-11 16:49:41 -0400

    Fri, 11 Jun 2021 20:49:41 GMT
    1 file changed, 31 insertions(+), 17 deletions(-)

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -5,24 +5,38 @@ import mailchimp from "../../utils/mailchimp";
 async function main(req, res) {
   const { firstName, lastName, email, postalCode } = req.body;
 
+  const opts = {
+    email_address: email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName,
+      POSTAL: postalCode,
+    },
+  };
+
   try {
-    await mailchimp.lists.updateListMember(MAILCHIMP_LIST_ID, md5(email), {
-      email_address: email,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName,
-        POSTAL: postalCode,
-      },
-    });
+    await mailchimp.lists.addListMember(MAILCHIMP_LIST_ID, opts);
 
-    return res.status(201).json({});
-  } catch (error) {
-    console.error({ error });
+    res.status(201).json({});
+  } catch (_error) {
+    console.error({ _error });
 
-    return res
-      .status(error.status)
-      .json({ errors: [JSON.parse(error.response.text).title] });
+    try {
+      await mailchimp.lists.updateListMember(
+        MAILCHIMP_LIST_ID,
+        md5(email),
+        opts
+      );
+
+      res.status(201).json({});
+    } catch (error) {
+      console.error({ error });
+
+      res
+        .status(error.status)
+        .json({ errors: [JSON.parse(error.response.text).title] });
+    }
   }
 }
 
