9a9a9aa458958baddf75fe0f7e62e7c950b82816
Jack Wilson <wils3005@gmail.com>
2021-08-19 14:38:50 -0400

    Thu, 19 Aug 2021 18:38:50 GMT
    1 file changed, 5 insertions(+), 8 deletions(-)

--- components/main.tsx
+++ components/main.tsx
@@ -5,30 +5,27 @@ import Footer from "./footer";
 import Header from "./header";
 import SignUpForUpdates from "./sections/sign-up-for-updates";
 
-const TITLE = "robicheau.ca";
+const TITLE =
+  " Nicole Robicheau | Let's lead with humanity | University-Rosedale, TO";
 
 const Main: FC = ({ children }: { children: ReactNode }) => {
   return (
     <main>
       <Head>
         <title>{TITLE}</title>
+        <meta name="robots" content="index, follow" />
         <link href="favicon.ico" rel="icon" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="apple-touch-icon.png"
-        />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="favicon-32x32.png"
+          href="favicon-orange-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="favicon-16x16.png"
+          href="favicon-orange-16x16.png"
         />
       </Head>
       <Header />
