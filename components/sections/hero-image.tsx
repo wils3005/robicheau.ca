a97572e00f50ee2170307ac00e01cfe2a5e695fe
Jack Wilson <wils3005@gmail.com>
2021-08-19 11:43:52 -0400

    Thu, 19 Aug 2021 15:43:52 GMT
    1 file changed, 12 insertions(+), 10 deletions(-)

--- components/sections/hero-image.tsx
+++ components/sections/hero-image.tsx
@@ -7,18 +7,20 @@ const HeroImage: FC = () => {
         alt="Nicole wearing a blue button down shirt smiling on a street with a colourful mural in the background"
         src="nicole-robicheau-outdoor-headshot-2800x1869.jpg"
       />
-      <div className="absolute flex flex-col items-end w-1/3">
-        <p className="bg-primary font-black my-1 lg:my-2 text-lg md:text-4xl lg:text-5xl text-white w-full">
-          Nicole
-          <br />
-          ROBICHEAU
-        </p>
-
-        <div className="bg-primary block my-1 lg:my-2 w-11/12">
-          <p className="text-shadow font-extrabold text-lg md:text-xl lg:text-2xl text-white">
-            Let&apos;s lead with humanity
+      <div className="absolute flex flex-col items-end w-1/3 mt-1 md:mt-2 lg:mt-3 xl:mt-4">
+        <div className="bg-primary">
+          <p className=" font-black my-1 lg:my-2 text-lg md:text-4xl lg:text-5xl text-white w-full">
+            Nicole
+            <br />
+            ROBICHEAU
+          </p>
+          <p className="font-bold text-md md:text-lg lg:text-xl text-secondary">
+            FOR UNIVERSITY-ROSEDALE
           </p>
         </div>
+        <p className="bg-primary text-shadow font-extrabold text-lg md:text-xl lg:text-2xl text-white mt-1 md:mt-2 lg:mt-3 xl:mt-4">
+          Let&apos;s lead with humanity
+        </p>
       </div>
     </section>
   );
